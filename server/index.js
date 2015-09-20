8d74c4a10a555190ba1531f59b9470261f39c7ba
wojtekswiderski <wswiders@uwaterloo.ca>
2015-09-19 20:20:37 -0400

    Websockets and Twilio

--- server/index.js
+++ server/index.js
@@ -3,15 +3,25 @@ var morgan = require('morgan');
 var express = require('express');
 var app = express();
 
+// Websocket stuff
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
 app.use(morgan('dev'));
 
 app.use(express.static('static'));
 
-app.get('/', function (req, res) {
-      res.send('Hello World!');
+app.get('/incoming', function (req, res) {
+	console.log('Received Incoming')
+	io.emit('twilioincoming', 'Hello World');
+	res.send('');
 });
 
-var server = app.listen(process.env.PORT || 3000, function () {
+io.on('connection', function(socket){
+	console.log('a user connected');
+});
+
+var server = http.listen(process.env.PORT || 3000, function () {
     var host = server.address().address;
     var port = server.address().port;
 
