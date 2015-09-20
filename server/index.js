58cd7f0254ffd9fa5f034b136f4bebe3df96e3f9
Shahmeer Navid <shahmeernavid@gmail.com>
2015-09-20 07:42:39 -0400

    test

--- server/index.js
+++ server/index.js
@@ -42,9 +42,9 @@ io.on('connection', function(socket){
     });
 });
 
-var server = http.listen(process.env.PORT || 3000, function () {
+var server = http.listen(8080, function () {
     var host = server.address().address;
     var port = server.address().port;
-    
+
     console.log('Example app listening at http://%s:%s', host, port);
 });
